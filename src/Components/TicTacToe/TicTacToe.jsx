76df837940e894c7f56819313fd9837d1345476b
Guneesh <guneesh.s@somaiya.edu>
2025-06-17 12:12:38 +0530

    casimng

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -1,9 +1,7 @@
 import React, { useRef, useState } from "react";
 import "./TicTacToe.css";
-import circle_icon from "../Assets/circle.png";
-import cross_icon from "../Assets/cross.png";
-
-//hi
+import circle_icon from "../assets/circle.png";
+import cross_icon from "../assets/cross.png";
 
 let data = ["", "", "", "", "", "", "", "", ""];
 
